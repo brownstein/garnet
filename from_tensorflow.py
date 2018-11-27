61edcf3a4d0360cf6d9b0be223e0dd4732058ce4
Robert Brownstein <robert.brownstein@gmail.com>
2018-11-26 21:00:06 -0500

    working

--- from_tensorflow.py
+++ from_tensorflow.py
@@ -6,7 +6,7 @@ from data.small_combinations import load_data
 
 (allData, allLabels) = load_data(
     10,
-    input_shape=(44, 44),
+    input_shape=(46, 46),
     label_shape=(38, 38)
     )
 
@@ -16,24 +16,26 @@ def loss(truth, prediction):
     return tf.reduce_sum(tf.math.squared_difference(truth
     , prediction))
 
-model = keras.models.load_model('./keras_saved/test')
+model = keras.models.load_model('./t1/t1.h5')
 
-model.compile(optimizer=tf.train.RMSPropOptimizer(0.001),
-              loss=loss,
-              metrics=['accuracy'])
-
-model.fit(tf.stack(allData, 0), tf.stack(allLabels, 0), epochs=3, steps_per_epoch=4)
-
-p = model.predict(tf.expand_dims(allData[0], axis=0), steps=1)
+model.load_weights('./t1/t1.h5', by_name=True)
 
 with tf.Session().as_default() as sess:
+
+    for w in model.weights:
+        sess.run(w.initializer)
+
+    for v in model.variables:
+        sess.run(v.initializer)
+
+    p = model.predict(tf.expand_dims(allData[5], axis=0), steps=1)
     s = tf.summary.image(name="out-p.png",
-                         tensor=p[:,:,:,:3])
+                         tensor=p[:,:,:,:1])
 
     summary = tf.summary.Summary.FromString(s.eval())
     png = summary.value[0].image.encoded_image_string
 
-    f = open("out1.png", "wb")
+    f = open("out2.png", "wb")
     f.write(png)
     f.close()
 
