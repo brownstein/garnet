abcf48a1c7f9aa55989d8763292eeb4b010f0c1b
Robert Brownstein <robert.brownstein@gmail.com>
2019-01-19 11:34:40 -0500

    Added the latest test cases

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import tensorflow as tf
 from tensorflow import keras
 from model import generateModel, linkWeights, unlinkWeights, copyWeights
 # from data.small_shapes_2 import load_dataset
-from data.small_combinations import load_dataset
+# from data.small_combinations import load_dataset
+from data.combined_shapes_2 import load_dataset
 from run_output import dump_images_2
 from loss import loss
 
-image_shape = (80, 80)
+image_shape = (64, 64)
 
 allDataAndLabels = load_dataset(dtype=tf.float16,
-                                input_shape=image_shape,
+                                data_shape=image_shape,
                                 label_shape=image_shape)
 
-validation_subset = allDataAndLabels.take(20)
+# validation_subset = allDataAndLabels.take(20)
 
 # load old model first so that the new one can initialize properly
 # oldModel = keras.models.load_model("garnet_r15.h5", compile=False)
@@ -66,8 +67,8 @@ with tf.Session().as_default() as sess:
     unlinkWeights(model, sess)
 
     # save the model
-    model.save_weights('./saved_models/garnet_r17', save_format='h5')
-    model.save("garnet_r17.h5")
+    model.save_weights('./saved_models/garnet_r18', save_format='h5')
+    model.save("garnet_r18.h5")
 
     # save output samples and exit
     dump_images_2(sess, model, allDataAndLabels, 1, 100)
