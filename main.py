65bd7ea801260623d9aadbbb0d66f07484994f3b
Robert Brownstein <robert.brownstein@gmail.com>
2019-01-30 15:07:35 -0500

    saving weights

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from model import generateModel, linkAllWeights, unlinkAllWeights
-from data.gestalt_shapes_2 import load_dataset as load_gestalt
+from data.gestalt_shapes_4 import load_dataset as load_gestalt
 from data.variably_sized_shapes import load_dataset as load_vsized
 
 from run_output import dump_images
@@ -33,23 +33,26 @@ variablySizedDataAndLabels = load_vsized(
     data_shape=image_shape,
     label_shape=image_shape,
     label_channels=label_channels,
-    repeat=False
+    repeat=True
 )
-allDataAndLabels = gestaltDataAndLabels.concatenate(variablySizedDataAndLabels)
-allDataAndLabels = allDataAndLabels.shuffle(100).repeat()
+
+allDataAndLabels = variablySizedDataAndLabels
+
+# allDataAndLabels = gestaltDataAndLabels.concatenate(variablySizedDataAndLabels)
+# allDataAndLabels = allDataAndLabels.shuffle(100).repeat()
 
 # build new model
 model = generateModel((image_shape[0], image_shape[1], 1),
                       output_filters=5,
                       initial_filters=8,
-                      depth=32
+                      depth=40
                       )
 
 with tf.Session().as_default() as sess:
     sess.run(tf.global_variables_initializer())
 
     # load weights from previous run
-    model.load_weights("./saved_models/garnet_rev_30_weights.h5", by_name=True)
+    model.load_weights("./saved_models/garnet_rev_31_weights.h5", by_name=True)
 
     # link repeated layers
     linkAllWeights(model)
@@ -70,7 +73,7 @@ with tf.Session().as_default() as sess:
 
     # do the math
     model.fit(allDataAndLabels,
-              epochs=1000,
+              epochs=100,
               steps_per_epoch=50,
               callbacks=[tensorboard]
               )
@@ -79,8 +82,8 @@ with tf.Session().as_default() as sess:
     unlinkAllWeights(model, sess)
 
     # save the model
-    model.save_weights('./saved_models/garnet_rev_30_weights.h5', save_format='h5')
-    model.save("./saved_models/garnet_rev_30_full.h5")
+    model.save_weights('./saved_models/garnet_rev_32_weights.h5', save_format='h5')
+    model.save("./saved_models/garnet_rev_32_full.h5")
 
     # dump output before weights are unlinked
     dump_images(sess, model, allDataAndLabels, 100, 0.3,
